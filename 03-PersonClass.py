dbd7ee645b0ae825f27d9cbcd1ae93d02f8dc008
Franco Sosuan <franco.sosuan@gmail.com>
2019-04-01 16:22:58 +0800

    Practice with classes

--- 03-PersonClass.py
+++ 03-PersonClass.py
@@ -3,7 +3,16 @@ class Person:
         self.first = first
         self.last = last
 
+    def full_name(self):
+        return self.first + " " + self.last
+
+    def formal_name(self,title):
+        return title + " " + self.full_name()
+
 
 person = Person("John","Smith")
 print(person.first)
 print(person.last)
+
+person_you = Person("Franco", "Sosuan")
+print(person_you.formal_name("Dr."))
