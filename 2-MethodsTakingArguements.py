92569df927a7996edac279f8f39b38abf49b885a
Franco Sosuan <franco.sosuan@gmail.com>
2019-04-01 15:52:57 +0800

    Logging text with time to log.txt

---
+++ 2-MethodsTakingArguements.py
@@ -0,0 +1,14 @@
+import time
+
+class FileLogger:
+    filename = "log.txt"
+
+    def write_message(self,message):
+        #This appends messages to log file
+        line = "{} {}\n" .format(int(time.time()),message)
+        with open(self.filename, "a") as outfile:
+            outfile.write(line)
+
+
+logger = FileLogger()
+logger.write_message("Hello")
