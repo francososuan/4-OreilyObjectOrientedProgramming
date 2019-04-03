6c158b41801bc65002ec8fc96e6bd169ceee2615
Franco Sosuan <franco.sosuan@gmail.com>
2019-04-03 10:57:36 +0800

    Exercise on Encapsulation

--- 02-MethodsTakingArguements.py
+++ 02-MethodsTakingArguements.py
@@ -4,7 +4,7 @@ class FileLogger:
     filename = "log.txt"
 
     def write_message(self,message):
-        #This appends messages to log file
+        # This appends messages to log file
         line = "{} {}\n" .format(int(time.time()),message)
         with open(self.filename, "a") as outfile:
             outfile.write(line)
