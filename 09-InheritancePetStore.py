91254434b8b273516ae0275b18c560323526a23f
Franco Sosuan <franco.sosuan@gmail.com>
2019-04-03 12:09:10 +0800

    Inheritance Pet Store

--- 09-InheritancePetStore.py
+++ 09-InheritancePetStore.py
@@ -21,7 +21,7 @@ class Cat_1:
 fido = Dog_1("Fido")
 print(fido.describe())
 
-
+print("")
 class Pet:
     def __init__(self,name):
         self.name = name
@@ -44,3 +44,29 @@ print(jiro.name + " " + jiro.describe())
 print(archer.name + " " + archer.describe())
 
 # Challenge: placing describe() into the Pet Class
+print("")
+
+
+class Pet_2:
+    sound = ""
+    def __init__(self,name):
+        self.name = name
+    def describe(self):
+        return print("the {} says: {}!" .format(self.__class__.__name__.lower(),self.sound))
+
+class Dog_2(Pet_2):
+    sound = "Woof"
+
+class Cat_2(Pet_2):
+    sound = "Meow"
+
+class Bird_2(Pet_2):
+    sound = "Chirp"
+
+mica = Dog_2("Mica")
+nyan = Cat_2("Nyan")
+tweety = Bird_2("Tweety")
+
+mica.describe()
+nyan.describe()
+tweety.describe()
