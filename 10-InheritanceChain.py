1fc26b2f1af34458cb3935846b989ee851750d1e
Franco Sosuan <franco.sosuan@gmail.com>
2019-04-03 11:06:49 +0800

    Inheritance Chain

---
+++ 10-InheritanceChain.py
@@ -0,0 +1,24 @@
+class Pet:
+    def __init__(self,name):
+        self.name = name
+
+class Dog(Pet):
+    def describe(self):
+        return "the dog says: Woof!"
+
+class LapDog(Dog):
+    def describe(self):
+        return "the lap dog says: Yip!"
+
+class LoudLapDog(LapDog):
+    def describe(self):
+        return "the loud lap dog says: YIP!"
+
+buck = Dog("Buck")
+print(buck.describe())
+
+shorty = LapDog("Shorty")
+print(shorty.describe())
+
+pip = LoudLapDog("Pip")
+print(pip.describe())
